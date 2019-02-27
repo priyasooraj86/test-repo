aeab1a6107b3a0a57abb4c5dbd924b1380479ca8
priyasooraj86 <priyasooraj86@gmail.com>
2019-02-27 09:59:11 +0530

    First commit before deployment

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { AppRoutingModule } from './app-routing.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
+import { PostsComponent } from './posts/posts.component';
+import { HomeComponent } from './home/home.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    PostsComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    AppRoutingModule,
+    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
